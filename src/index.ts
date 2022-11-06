51342fc24c69cea76a050109363aa37adf9bee22
tirth-2001 <tirthgpatel.27@gmail.com>
2022-11-06 17:22:09 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express'
+
+const app = express()
+const port = process.env.PORT || 8080
+
+app.get('/', (_req: Request, res: Response) => {
+	return res.send('Express Typescript on Vercel')
+})
+
+app.get('/ping', (_req: Request, res: Response) => {
+	return res.send('pong 🏓')
+})
+
+app.listen(port, () => {
+	return console.log(`Server is listening on ${port}`)
+})
